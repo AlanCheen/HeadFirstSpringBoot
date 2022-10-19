82502633300abc82ba8508519ecd2169810b94cd
mingjue <zhening.czn@alibaba-inc.com>
2022-10-19 15:56:17 +0800

    init

---
+++ src/main/java/me/yifeiyuan/hf/spring/boot/HeadFirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.yifeiyuan.hf.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yifeiyuan/hf/spring/boot/HeadFirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yifeiyuan.hf.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
